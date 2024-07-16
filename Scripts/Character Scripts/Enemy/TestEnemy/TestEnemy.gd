70fbd7bafb82f887ead57f0572d8983c27df9176
Lucas Hartley <LukeIsPhoenix@gmail.com>
2024-07-15 20:15:10 -0700

    create test enemy

---
+++ Scripts/Character Scripts/Enemy/TestEnemy/TestEnemy.gd
@@ -0,0 +1,11 @@
+extends CharacterBody2D
+
+@export var HP = 10
+
+func _physics_process(delta):
+	if HP <= 0:
+		self.queue_free()
+
+func take_damage(damage_amount):
+	print("Damage: ", damage_amount)
+	HP -= damage_amount
